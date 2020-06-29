014647e8a779a3da41236183942fee7b04df9924
Anthony Fabian <anthony.fabian88@gmail.com>
2020-06-29 19:57:47 -0400

    The calculations will be done in python, not c#

--- Grapher.cs
+++
@@ -1,7 +0,0 @@
-using System;
-
-// The function to graph.
-private float F(float x)
-{
-    return (float)((1 / (2 * x) - 1 / (x - 1) + 3 * x * x) / 12);
-}
---
+++ Grapher.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib as plt
+
+def f(x):
+    return x
+
