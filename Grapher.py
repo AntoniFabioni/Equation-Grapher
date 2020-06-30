1b618b1f50321a9315bc25189f7b08cdec472158
Anthony Fabian <anthony.fabian88@gmail.com>
2020-06-30 16:12:22 -0400

    Added ability to draw axes

--- Grapher.py
+++ Grapher.py
@@ -1,6 +1,21 @@
 import numpy as np
 import matplotlib as plt
+import turtle
+
+t = turtle.Turtle()
+
+def drawAxes(width, height):
+    t.penup()
+    t.goto(-width/2, 0)
+    t.pendown()
+    t.goto(width/2, 0)
+
+    t.penup()
+    t.goto(0, -height/2)
+    t.pendown()
+    t.goto(0, height/2)
 
 def f(x):
     return x
 
+drawAxes(200,200)
