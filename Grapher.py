ad75c67412f52ff6ec6facf9de0c8c7e99fc5902
Anthony Fabian <anthony.fabian88@gmail.com>
2020-07-04 12:03:57 -0400

    Added comments

--- Grapher.py
+++ Grapher.py
@@ -7,11 +7,13 @@ t = turtle.Turtle()
 # 1 to 10 ramps up speed, but 0 is fastest speed.
 t.speed(0)
 
+# domain, range of values displayed is [-width/2, width/2], [-height/2, height/2].
 width, height = 400, 400
 
 # How many units are the gridlines spaced apart?
 gridSpacing = 10
 
+# Any function y = f(x) will work here.
 def f(x):
     return (150 / (1 + np.exp(20 - x / 4)) - 60) - (120 / (1 + np.exp(30 + x / 2)) - 20)
 
